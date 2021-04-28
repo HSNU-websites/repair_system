9cf7fbef7cfb95386d10f6eac25191ae1126f7b4
SiriusKoan <k.peihsun@gmail.com>
2021-04-28 22:28:35 +0800

    add app factory mode structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 from flask import Blueprint, Flask
 from flask_login import LoginManager
-from config import config
+from .config import config
 
 login_manager = LoginManager()
 
+
 def create_app(env):
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../templates", static_folder="../static")
     app.config.from_object(config[env])
-    
+
     login_manager.init_app(app)
     
+    from .main import main_bp
+    app.register_blueprint(main_bp)
+    
+    from .user import user_bp
+    app.register_blueprint(user_bp)
+    
+    from .admin import admin_bp
+    app.register_blueprint(admin_bp)
+
     return app
