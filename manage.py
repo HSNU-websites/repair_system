a52230e780158282398f2a67c317de1ff5b2fcb7
SiriusKoan <k.peihsun@gmail.com>
2021-04-30 14:13:27 +0000

    add test

--- manage.py
+++ manage.py
@@ -1,8 +1,14 @@
+import unittest
 from app import create_app
 from flask_script import Manager
 
 app = create_app("production")
 manager = Manager(app)
+
+@manager.command
+def test():
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == "__main__":
     manager.run()
---
+++ tests/test_basic.py
@@ -0,0 +1,13 @@
+import unittest
+from flask import url_for
+from app import create_app
+
+class BasicTest(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app("testing")
+        self.client = app.test_client()
+    def tearDown(self):
+        pass
+    def test_alive(self):
+        r = self.client.get("main.index_page")
+        self.assertTrue(r.status_code == 200)
