1341e6c2000a3e6e43c83d973410961976df06d2
Restyled.io <commits@restyled.io>
2021-05-01 12:08:22 +0000

    Restyled by yapf

--- database.py
+++ database.py
@@ -47,7 +47,10 @@ class Admins(db.Model):
 class Users(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(16), unique=True, nullable=False, index=True)
+    username = db.Column(db.String(16),
+                         unique=True,
+                         nullable=False,
+                         index=True)
     # hashlib.sha256("pAs$W0rd".encoding("utf-8")).hexdigest()
     password = db.Column(db.CHAR(64), nullable=False)
     name = db.Column(db.String(255), nullable=False)
@@ -81,13 +84,15 @@ class Records(db.Model):
     item_id = db.Column(db.ForeignKey("items.id"), nullable=False)
     building_id = db.Column(db.ForeignKey("buildings.id"), nullable=False)
     location = db.Column(db.String(255), nullable=False)
-    time = db.Column(
-        db.TIMESTAMP, server_default=db.func.now(), nullable=False, index=True
-    )
+    time = db.Column(db.TIMESTAMP,
+                     server_default=db.func.now(),
+                     nullable=False,
+                     index=True)
     description = db.Column(db.String(255), nullable=False)
     revisions = db.relationship("Revisions")
 
-    def __init__(self, user_id, item_id, building_id, location, time, description):
+    def __init__(self, user_id, item_id, building_id, location, time,
+                 description):
         self.user_id = user_id
         self.item_id = item_id
         self.building_id = building_id
@@ -102,9 +107,10 @@ class Revisions(db.Model):
     record_id = db.Column(db.ForeignKey("records.id"), nullable=False)
     admin_id = db.Column(db.ForeignKey("users.id"), nullable=False)
     status_id = db.Column(db.ForeignKey("statuses.id"), nullable=False)
-    time = db.Column(
-        db.TIMESTAMP, server_default=db.func.now(), nullable=False, index=True
-    )
+    time = db.Column(db.TIMESTAMP,
+                     server_default=db.func.now(),
+                     nullable=False,
+                     index=True)
     description = db.Column(db.String(255), nullable=False)
 
     def __init__(self, record_id, admin_id, status_id, time, description):
