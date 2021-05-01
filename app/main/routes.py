a8f7fcfac39709b9511d972cb3d4e58981d0d8dd
SiriusKoan <k.peihsun@gmail.com>
2021-05-01 12:12:16 +0800

    add report.html

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 from flask import request
 from flask_login import current_user
 from flask_login import UserMixin
@@ -21,8 +21,11 @@ def load_user(username):
 
 @main_bp.route("/", methods=["GET", "POST"])
 def index_page():  # index page is login page
-    if request.method == "GET":
-        return render_template("index.html", form=LoginForm())
-    if request.method == "POST":
-        # auth user
-        pass
+    if current_user.is_active:
+        return redirect(url_for("user.report_page"))
+    else:
+        if request.method == "GET":
+            return render_template("index.html", form=LoginForm())
+        if request.method == "POST":
+            # auth user
+            pass
--- app/user/routes.py
+++ app/user/routes.py
@@ -1,1 +1,10 @@
 from . import user_bp
+from app import user
+
+@user_bp.route("/report")
+def report_page():
+    pass
+
+@user_bp.route("/dashboard")
+def dashboard_page():
+    pass
