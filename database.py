0f9c0b163adf0f611d7ece79d72f2dad1ec07aa7
william920429 <william920429@gmail.com>
2021-05-01 18:30:56 +0800

    database relationship

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 
@@ -12,8 +11,8 @@ class Statuses(db.Model):
         self.description = description
 
 
-class Types(db.Model):
-    __tablename__ = 'types'
+class Items(db.Model):
+    __tablename__ = 'items'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     description = db.Column(db.String(255), nullable=False)
 
@@ -32,59 +31,65 @@ class Buildings(db.Model):
 ################################################################
 
 
+class Admins(db.Model):
+    __tablename__ = "admins"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.ForeignKey("users.id"), unique=True, nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, user_id, email):
+        self.user_id = user_id
+        self.email = email
+
+
 class Users(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(16), unique=True,
                          nullable=False, index=True)
-    # hashlib.sha256(string.encoding("utf-8")).hexdigest()
+    # hashlib.sha256("pAs$W0rd".encoding("utf-8")).hexdigest()
     password = db.Column(db.CHAR(64), nullable=False)
     name = db.Column(db.String(255), nullable=False)
     classnum = db.Column(db.Integer, nullable=False)
+    # bool(admin) to check if it is admin
+    admin = db.relationship("Admins", uselist=False)
 
     def __init__(self, username, password, name, classnum):
         self.username = username
         self.password = password
         self.name = name
         self.classnum = classnum
+    
+    def grant_admin(self, email = ""):
+        if self.admin:
+            self.admin.email = email
+        else:
+            self.admin = Admins(self.id, email)
+        db.session.commit()
 
-
-class Admins(db.Model):
-    __tablename__ = "admins"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(16), unique=True,
-                         nullable=False, index=True)
-    password = db.Column(db.String(16), nullable=False)
-    name = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
-
-    def __init__(self, username, password, name, email):
-        self.username = username
-        self.password = password
-        self.name = name
-        self.email = email
+    def revoke_admin(self):
+        if self.admin:
+            db.session.delete(self.admin)
+            db.session.commit()
 
 
 class Records(db.Model):
     __tablename__ = "records"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    type_id = db.Column(db.Integer, db.ForeignKey('types.id'), nullable=False)
-    building_id = db.Column(db.Integer, db.ForeignKey(
-        'buildings.id'), nullable=False)
+    user_id = db.Column(db.ForeignKey('users.id'), nullable=False)
+    item_id = db.Column(db.ForeignKey('items.id'), nullable=False)
+    building_id = db.Column(db.ForeignKey('buildings.id'), nullable=False)
     location = db.Column(db.String(255), nullable=False)
-    status_id = db.Column(db.Integer, db.ForeignKey(
-        'statuses.id'), nullable=False)
-    time = db.Column(db.DateTime, nullable=False,
+    time = db.Column(db.TIMESTAMP, server_default=db.func.now(), nullable=False,
                      index=True)
     description = db.Column(db.String(255), nullable=False)
+    revisions = db.relationship("Revisions")
 
-    def __init__(self, user_id, type_id, building_id, location, status_id, time, description):
+    def __init__(self, user_id, item_id, building_id, location, time, description):
         self.user_id = user_id
-        self.type_id = type_id
+        self.item_id = item_id
         self.building_id = building_id
         self.location = location
-        self.status_id = status_id
         self.time = time
         self.description = description
 
@@ -92,13 +97,11 @@ class Records(db.Model):
 class Revisions(db.Model):
     __tablename__ = 'revisions'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    record_id = db.Column(db.Integer, db.ForeignKey(
-        'records.id'), nullable=False)
-    admin_id = db.Column(db.Integer, db.ForeignKey(
-        'admins.id'), nullable=False)
-    status_id = db.Column(db.Integer, db.ForeignKey(
-        'statuses.id'), nullable=False)
-    time = db.Column(db.DateTime, nullable=False)
+    record_id = db.Column(db.ForeignKey('records.id'), nullable=False)
+    admin_id = db.Column(db.ForeignKey('users.id'), nullable=False)
+    status_id = db.Column(db.ForeignKey('statuses.id'), nullable=False)
+    time = db.Column(db.TIMESTAMP, server_default=db.func.now(), nullable=False,
+                     index=True)
     description = db.Column(db.String(255), nullable=False)
 
     def __init__(self, record_id, admin_id, status_id, time, description):
--- dbtest.py
+++ dbtest.py
@@ -1,17 +1,16 @@
 from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
 from database import *
-from db_credential import db_user, db_pass, db_name
 from flask_script import Manager, Server
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://{username}:{password}@localhost/{db_name}".format(
-    username=db_user, password=db_pass, db_name=db_name)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://{username}:{password}@localhost/{db_name}".format(
+#     username=db_user, password=db_pass, db_name=db_name)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
-# 設定你的 app
 manager = Manager(app)
 # 設定 python manage.py runserver 為啟動 server 指令
 manager.add_command('runserver', Server())
@@ -20,7 +19,7 @@ manager.add_command('runserver', Server())
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, Users=Users, db=db)
+    return globals()
 
 
 if __name__ == '__main__':
