683961c35cfff927e2940085432c51a90ee255b9
william920429 <william920429@gmail.com>
2021-05-03 00:33:01 +0800

    add login_auth

--- app/db_helper.py
+++ app/db_helper.py
@@ -1,4 +1,5 @@
 from .database import db, Statuses, Items, Buildings, Admins, Users, Records, Revisions
+from hashlib import sha256
 
 def render_statuses():
     statuses = Statuses.query.all()
@@ -11,3 +12,10 @@ def render_items():
 def render_buildings():
     buildings = Buildings.query.all()
     return [building.description for building in buildings]
+
+def login_auth(username, password):
+    user = Users.query.filter_by(username=username).first()
+    if user and user.password == sha256(password.encode('utf8').hexdigest()):
+        return dict(id = Users.id, isAdmin=bool(Users.admin))
+    else:
+        return False
