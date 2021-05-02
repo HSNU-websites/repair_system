5994c1b8d93541b49bca82e39ba2cc493d7b53d7
Restyled.io <commits@restyled.io>
2021-05-02 03:47:00 +0000

    Restyled by yapf

--- db_shell.py
+++ db_shell.py
@@ -8,13 +8,12 @@ from database import *
 
 app = Flask(__name__)
 app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}".format(
-    DB_USER=getenv("DB_USER"),
-    DB_PASSWORD=getenv("DB_PASS"),
-    DB_HOST=getenv("DB_HOST", "localhost"),
-    DB_DATABASE=getenv("DB_NAME"),
-)
+    "SQLALCHEMY_DATABASE_URI"] = "mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}".format(
+        DB_USER=getenv("DB_USER"),
+        DB_PASSWORD=getenv("DB_PASS"),
+        DB_HOST=getenv("DB_HOST", "localhost"),
+        DB_DATABASE=getenv("DB_NAME"),
+    )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 manager = Manager(app)
@@ -29,11 +28,9 @@ def make_shell_context():
 @manager.command
 def reset():
     "Reset All Tables to Default."
-    print(
-        "This will drop all tables.\n"
-        "It's extremely dangerous.\n"
-        "If you are sure, please type 'YES!'"
-    )
+    print("This will drop all tables.\n"
+          "It's extremely dangerous.\n"
+          "If you are sure, please type 'YES!'")
     a = input()
     if a != "YES!":
         return 1
