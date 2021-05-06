05e932cf77abcd3d06ae8d72f17c858e05f83117
william920429 <william920429@gmail.com>
2021-05-06 16:35:41 +0800

    Database: change password hash method

--- app/db_helper.py
+++ app/db_helper.py
@@ -1,16 +1,21 @@
-from .database import db, Statuses, Items, Buildings, Users, Records, Revisions, Unfinished
 from hashlib import sha256
-from passlib.context import CryptContext
-# pbkdf2_sha256
-# verify_and_update
+from os import urandom
+from base64 import b64encode, b64decode
 
-passwd_context = CryptContext( # First scheme will be default
+from passlib.context import CryptContext
+
+from .database import (Buildings, Items, Records, Revisions, Statuses,
+                       Unfinished, Users, db)
+
+passwd_context = CryptContext(  # First scheme will be default
     schemes=["pbkdf2_sha256", "sha512_crypt"],
     deprecated="auto"
 )
 
+
 def render_statuses():
-    statuses = db.session.query(Statuses.description).order_by(Statuses.sequence).all()
+    statuses = db.session.query(
+        Statuses.description).order_by(Statuses.sequence).all()
     return [status.description for status in statuses]
 
 
@@ -21,7 +26,8 @@ def render_items():
 
 def render_buildings():
     buildings = (
-        db.session.query(Buildings.description).order_by(Buildings.sequence).all()
+        db.session.query(Buildings.description).order_by(
+            Buildings.sequence).all()
     )
     return [building.description for building in buildings]
 
@@ -36,21 +42,31 @@ def get_admin_emails():
     return [admin.email for admin in admins]
 
 # need revision
+
+
 def login_auth(username, password):
     user = Users.query.filter_by(username=username).first()
-    if user and (t:=passwd_context.verify_and_update(password, user.password))[0]:
-        # verify_and_update() return (True|false, str|None)    
-        if t[1] is not None:
+
+    # verify_and_update() returns tuple (verified: bool, new_hash: str|None)
+    if user and (t := passwd_context.verify_and_update(password, user.password))[0]:
+        if t[1]:
             user.password = t[1]
             db.session.commit()
         return dict(id=user.id, isAdmin=user.isAdmin())
     else:
         return False
 
-# not finished
+
 def updateUnfinished():
-    finished =1000 ####
+    finishedStatus_id = 2
     Unfinished.__table__.drop(db.session)
+    Unfinished.__table__.create(db.session)
+    l = []
     for record in Records.query.all():
-        if record and record.revisions and record.revisions[-1].id == finished:
-           pass
+        if not (record.revisions and record.revisions[-1].status_id == finishedStatus_id):
+            l += Unfinished(record.id)
+    db.session.bulk_save_objects(l)
+    db.session.commit()
+
+def generateVerificationCode(user_id:int)->str:
+    return b64encode(urandom(32))
