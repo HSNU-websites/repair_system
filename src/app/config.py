63f420919eb9c03033239db2ea58f0bf8da13b27
william920429 <william920429@gmail.com>
2021-07-17 00:18:26 +0800

    restyle config

--- src/app/config.py
+++ src/app/config.py
@@ -2,55 +2,65 @@ from os import getenv, urandom
 
 
 class Config:
-    SECRET_KEY = urandom(32)
-    # SQLAlchemy
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}?charset=utf8mb4".format(
-        DB_USER=getenv("DB_USER"),
-        DB_PASSWORD=getenv("DB_PASSWORD"),
-        DB_HOST=getenv("DB_HOST"),
-        DB_DATABASE=getenv("DB_DATABASE"),
-    )
-    # Mail
+    # Flask-Caching
+    CACHE_DEFAULT_TIMEOUT = 0  # Never timeout
+    CACHE_THRESHOLD = 5000
+    # Flask-Mail
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USERNAME = getenv("MAIL_USERNAME")
     MAIL_PASSWORD = getenv("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = "HSNU"
-    # APScheduler
-    SCHEDULER_API_ENABLED = True
-    # Cache
-    CACHE_TYPE = "FileSystemCache"
-    CACHE_DIR = "/dev/shm/repair_system/"
-    CACHE_DEFAULT_TIMEOUT = 0  # Never timeout
-    CACHE_THRESHOLD = 5000
-    # reCAPTCHA
+    # Flask-SQLAlchemy
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Flask-WTF reCAPTCHA
     RECAPTCHA_PUBLIC_KEY = getenv("RECAPTCHA_PUBLIC_KEY")
     RECAPTCHA_PRIVATE_KEY = getenv("RECAPTCHA_PRIVATE_KEY")
 
 
 class Development(Config):
+    # Flask
     ENV = "development"
+    DEBUG = True
     SECRET_KEY = "dhfkjgruytv4yntcm94[g"
+    # Flask-APScheduler
+    SCHEDULER_API_ENABLED = True
+    # Flask-SQLAlchemy
     SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
     CACHE_TYPE = "SimpleCache"
-    DEBUG = True
 
 
 class Testing(Config):
+    # Flask
     ENV = "testing"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-    CACHE_TYPE = "SimpleCache"
-    TESTING = True
     DEBUG = True
     PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SECRET_KEY = "dhfkjgruytv4yntcm94[g"
+    TESTING = True
+    # Flask-Caching
+    CACHE_TYPE = "SimpleCache"
+    # Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    # Flask-WTF
     WTF_CSRF_ENABLED = False
 
 
 class Production(Config):
+    # Flask
     ENV = "production"
     PREFERRED_URL_SCHEME = "https"
+    SECRET_KEY = urandom(32)
+    # Flask-Caching
+    CACHE_TYPE = "FileSystemCache"
+    CACHE_DIR = "/dev/shm/repair_system/"
+    # Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = "mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}?charset=utf8mb4".format(
+        DB_USER=getenv("DB_USER"),
+        DB_PASSWORD=getenv("DB_PASSWORD"),
+        DB_HOST=getenv("DB_HOST"),
+        DB_DATABASE=getenv("DB_DATABASE"),
+    )
 
 
 config = {
