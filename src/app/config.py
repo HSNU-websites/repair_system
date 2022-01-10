3cd525dbc5b64c0d20fee2266c5fb256ae59ce88
SiriusKoan <k.peihsun@gmail.com>
2022-01-10 20:30:25 +0800

    Revert "Deprecate flask-script"

--- src/app/config.py
+++ src/app/config.py
@@ -39,7 +39,6 @@ class Testing(Config):
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SECRET_KEY = "dhfkjgruytv4yntcm94[g"
     TESTING = True
-    SERVER_NAME = "localhost"
     # Flask-Caching
     CACHE_TYPE = "NullCache"
     # Flask-SQLAlchemy
